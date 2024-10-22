1b7133702e3bcefc5d9d310ba6e3ae429f955c1b
zeck <hellozeck.eth@gmail.com>
2024-10-22 22:46:49 +0800

    fix ready

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 require('dotenv').config();
-
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        // 添加其他所需的 intents
+        GatewayIntentBits.MessageContent,
+        // Add other required intents here
     ]
 });
 
-client.login(process.env.DISCORD_TOKEN);
+// Create a collection to store all events
+client.events = new Collection();
+
+// Read all files in the events directory
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+    client.events.set(event.name, event);
+}
+
+client.login(process.env.DISCORD_TOKEN)
+    .then(() => console.log('Login successful'))
+    .catch(error => console.error('Login failed:', error));
 
 process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
